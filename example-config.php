bfba215177e294ae46af95ce269d87468fa2196e
Ben Greene <benrgreene@gmail.com>
2019-01-16 15:25:55 -0800

    chore: update example config
    Removed the DB schema from the example config file (in its own file now)
    And added the site title definition to config

--- example-config.php
+++ example-config.php
@@ -6,6 +6,8 @@
  * ------------------------------------------------
  */
 // Base site info
+define( 'SITE_TITLE', 'Site Name');
+// This is the name of the theme directory that should be active
 define( 'THEME_NAME', 'theme-default' );
 // This is the number of posts per page
 define( 'PAGE_SIZE', 10 );
@@ -19,53 +21,3 @@ define( 'DB_NAME', 'database' );
 define( 'DB_USERNAME', 'root' );
 // Database user password
 define( 'DB_PASSWORD', 'root' );
-
-/**
- * ------------------------------------------------
- * Database Schema (DON'T EDIT)
- * ------------------------------------------------
- */
-// Database version
-define( 'DB_VERSION', 7 );
-// Schema
-define( 'DB_SCHEMA', array(
-  'users' => array(
-    'email'    => 'varchar(60)',
-    'name'     => 'varchar(60)',
-    'password' => 'varchar(255)',
-    'level'    => 'varchar(20)'
-  ),
-  'posts' => array(
-    'slug'      => 'varchar(60)',
-    'type'      => 'varchar(60)',
-    'title'     => 'varchar(60)',
-    'author'    => 'varchar(60)',
-    'excerpt'   => 'longtext',
-    'content'   => 'longtext',
-    'published' => 'varchar(20)',
-    'date'      => 'datetime',
-  ),
-  'post_meta' => array(
-    'post_id' => 'bigint(20)',
-    'content' => 'longtext',
-  ),
-  'options' => array(
-    'name'  => 'varchar(20)',
-    'value' => 'longtext',
-  ),
-  'tokens'  => array(
-    'token'      => 'varchar(60)',
-    'expiration' => 'datetime',
-  ),
-  'theme_options' => array(
-    'name'  => 'varchar(60)',
-    'value' => 'longtext',
-  ),
-  'uploads' => array(
-    'name'  => 'varchar(60)',
-  ),
-  'related_upload' => array(
-    'post_id'   => 'bigint(20)',
-    'upload_id' => 'bigint(20)',
-  )
-) );
